c2318ddfe283aff006319e11d6bcf867fc8a80e2
AnkitChaudhary07 <ankichaudhary126@gmail.com>
2024-08-22 10:13:40 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>One Ant</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f85b5b",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
